83454a690df87543a9bdb39492f41ec01fa36c00
Brett Naul <brettnaul@gmail.com>
2017-10-25 22:04:14 -0700

    Shorten sleep time

--- docker_build.sh
+++ docker_build.sh
@@ -7,9 +7,8 @@ docker build -t skyportal/web .
 
 # Run tests before uploading
 docker-compose up &
-# TODO this can be reduced to ~15s once `run_production` is changed to skip `npm install`
-echo "Sleeping for 180 seconds while server starts"
-sleep 180
+echo "Sleeping for 60 seconds while server starts"
+sleep 60
 
 echo -e "\nports:\n    app: 9000" >> test_config.yaml
 PYTHONPATH=. xvfb-run -a pytest -v skyportal/tests/frontend/test_remote.py
